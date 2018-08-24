60fb287b59aae62e6eaad54ae9f01f44641db1b4
Bruno Santana <brunosantana@MacBook-Pro-de-Bruno.local>
2018-08-24 18:40:11 -0300

    feat(master): ajustes - atualizando next - refactor app.js - atualizacao do README.md

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 
-class App extends Component {
-    render() {
-        
-        const Title = styled.h1`
-            color: red;
-            font-size: 60px;
-        `;
+const Title = styled.h1`
+    color: red;
+    font-size: 60px;
+`;
 
-        return (
-            <React.Fragment>
-                <Title>React boileplate</Title>
-                <div>React 16</div>,
-                <p>Working...</p>
-            </React.Fragment>
-        );
-    }
+class App extends PureComponent {
+    render = () => (
+        <React.Fragment>
+            <Title>React boileplate</Title>
+            <div>React 16</div>,
+            <p>Working...</p>
+        </React.Fragment>
+    )
 }
 
 export default App;
